a9fa1bd8aea4dcf7998483e36edfe50d40b7d1a4
Luedvi <luedvi@outlook.com>
2024-10-06 12:49:32 -0600

    Create wsl.sh

---
+++ wsl.sh
@@ -0,0 +1,13 @@
+# wslview: is the equivalent of xdg-open for wsl distributions
+wslview file.txt
+wslview directory
+
+# we can also open the windows terminal and open the files from there by typing the path of the file. To exit the cmd just type "exit"
+cmd.exe
+
+# or open the windows file manager
+explorer.exe
+explorer.exe directory
+
+# we can install wsl-open as an alternative
+npm install -g wsl-open
